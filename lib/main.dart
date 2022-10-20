10af68aa70f0cf8a920345ee4e78b496816b2de1
Andy Keobounphan <andykeo@live.ca>
2022-10-19 20:49:54 -0600

    Created a link on the login page to route to register page.
    Linked the login page to the register page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:noteking/views/login_view.dart';
+import 'package:noteking/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -13,6 +15,10 @@ void main() {
         primarySwatch: Colors.red,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -34,14 +40,15 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              // Grab the current user that's logged in
-              final user = FirebaseAuth.instance.currentUser;
-              // If the user is verified take the value, otherwise send them to verifyView
-              if (user?.emailVerified ?? false) {
-                return const Text('Done');
-              } else {
-                return const VerifyEmailView();
-              }
+              // // Grab the current user that's logged in
+              // final user = FirebaseAuth.instance.currentUser;
+              // // If the user is verified take the value, otherwise send them to verifyView
+              // if (user?.emailVerified ?? false) {
+              //   return const Text('Done');
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
             default:
               return const Text('Loading...');
           }
