4ec79687b1e997eeddba83154ae2d43f8bb7812a
Andy Keobounphan <andykeo@live.ca>
2022-10-22 23:51:22 -0600

    Created separate exception classes and an auth_provider class.

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -63,6 +63,8 @@ class _RegisterViewState extends State<RegisterView> {
                     email: email, password: password);
                 final user = FirebaseAuth.instance.currentUser;
                 await user?.sendEmailVerification();
+                if (!mounted) return;
+                Navigator.of(context).pushNamed(verifyEmailRoute);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
                   await showErrorDialog(
